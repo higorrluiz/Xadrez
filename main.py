8e539063334c04889f465e01f19742f3427fa99b
joao-loyola <joaoloyola@id.uff.br>
2023-06-25 13:10:59 -0300

    aviso rei em xeque
    Mostra visualmente quando o rei está em xeque e qual foi o último movimento. Espera 2 segundos antes de mostrar a tela de fim de jogo.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from time import sleep
 import pygame 
 from pygame.locals import *
 from sys import exit
 from classes.piece import Piece
 from classes.pawn import Pawn
 from classes.queen import Queen
+from classes.king import King
+from classes.board import Board
 from classes.match import Match
-from board_helper import *
+from helper import *
+from importador import *
 from menu import Menu
 from ai import ChessPlayer
-from importador import STATE_PATH
 
 
 def get_config(arq: str) -> tuple[list[bool], int]:
@@ -35,7 +38,7 @@ def desenha_tela(tabuleiro: Board) -> None:
 
 
 def end_turn() -> tuple[bool, list[Piece], str, str]:
-    global game_state, jogo, tabuleiro, white_turn, pecas, winner
+    global game_state, jogo, tabuleiro, white_turn, pecas, white_check, black_check, winner, end
     if white_turn:
         jogo.passant_black = None
     else:
@@ -44,23 +47,28 @@ def end_turn() -> tuple[bool, list[Piece], str, str]:
     white_turn = not white_turn
 
     check = jogo.king_is_checked(white_turn)
+    if check:
+        if white_turn: white_check = True
+        else: black_check = True
     pecas = tabuleiro.get_pieces(white_turn)
     for p in pecas:
         p.possible_moves(check)
-        
+
     if jogo.is_checkmate(white_turn, check):
         game_state = "checkmate"
         menu.game_state = game_state
         winner = "black" if white_turn else "white"
-    
+        end = True
     if jogo.is_tie(white_turn, check):
         game_state = "tie"
         menu.game_state = game_state
+        end = True
+    desenha_tela(tabuleiro)
 
 
 def player() -> None:
-    global game_state, menu, jogo, tabuleiro, white_turn, check, has_ia, player_is_white, ia_difficulty, \
-        peca, pecas, movimentos_validos, sel, winner
+    global game_state, menu, jogo, tabuleiro, white_turn, check, has_ia, player_is_white, ia_difficulty, peca, pecas, \
+        movimentos_validos, sel, old_pos, new_pos, w_king_pos, b_king_pos, white_check, black_check, winner, end
     
     pygame.event.set_blocked(pygame.MOUSEMOTION)
     for event in pygame.event.get():
@@ -91,28 +99,45 @@ def player() -> None:
                 x, y = posicao_do_quadrado()
                 sel = mouse_pos
                 if (x, y) in movimentos_validos:
-                    peca.move((7-round(y/tamanho), round(x/tamanho)))
+                    old_pos = peca.get_pos()
+                    peca.move((7-round(y/TAMANHO), round(x/TAMANHO)))
+                    new_pos = peca.get_pos()
+                    
+                    white_check = black_check = False
+                    if isinstance(peca, King):
+                        if white_turn: w_king_pos = new_pos
+                        else: b_king_pos = new_pos
+
                     peca.selecionado = False
                     movimentos_validos = []
                     if isinstance(peca, Pawn) and peca.get_row() in [0, 7]: 
                         menu.promotion(peca, white_turn)
                     end_turn()
-                    desenha_tela(tabuleiro)
 
     if show_possible_moves:
         for (x, y) in movimentos_validos:
-            pygame.draw.circle(tela, (207,14,14), (x+tamanho/2, y+tamanho/2), 10)
+            pygame.draw.circle(tela, (207,14,14), (x+TAMANHO/2, y+TAMANHO/2), 10)
     selecionado(sel)
+    if old_pos is not None:
+        selecionado_cor(old_pos, (0, 255, 255))
+        selecionado_cor(new_pos, (0, 255, 255))
+    if white_check: selecionado_cor(w_king_pos, (255, 0, 0))
+    if black_check: selecionado_cor(b_king_pos, (255, 0, 0))
 
 
 game_loop = True
 game_state = "menu"
 menu = Menu(tela, game_loop, game_state)
 show_possible_moves = True
+player_is_white = True
 ia_toggled = False
 ia_difficulty = 0
+end = False
 
 while game_loop:
+    if end: 
+        sleep(2)
+        end = False
     tela.fill('black')
     if game_state == "menu":
         game_loop, game_state = menu.draw()
@@ -123,9 +148,14 @@ while game_loop:
     elif game_state == "tie":
         game_loop, game_state = menu.tie()
     elif game_state == "continue_game":
-        tabuleiro: Board = Board(tela, tamanho, STATE_PATH)
-        jogo: Match = Match(tabuleiro, STATE_PATH)
         sel = (20000, 30000)
+        old_pos = new_pos = None
+        w_king_pos = (0, 4)
+        b_king_pos = (7, 4)
+        white_check = black_check = False
+
+        tabuleiro: Board = Board(tela, TAMANHO, STATE_PATH)
+        jogo: Match = Match(tabuleiro, STATE_PATH)
         peca: Piece = Piece()
         (white_turn, check, has_ia, player_is_white), ia_difficulty = get_config(STATE_PATH)
         movimentos_validos = []
@@ -143,9 +173,14 @@ while game_loop:
         # deleta estado salvo
         open(STATE_PATH, 'w').close()
 
-        tabuleiro: Board = Board(tela, tamanho)
-        jogo: Match = Match(tabuleiro)
         sel = (20000, 30000)
+        old_pos = new_pos = None
+        w_king_pos = (0, 4)
+        b_king_pos = (7, 4)
+        white_check = black_check = False
+
+        tabuleiro: Board = Board(tela, TAMANHO)
+        jogo: Match = Match(tabuleiro)
         peca: Piece = Piece()
         white_turn = True
         check = False
@@ -157,6 +192,8 @@ while game_loop:
         for p in pecas:
             p.possible_moves(check)
         game_state = "game"
+        ia_toggled = False
+
     elif game_state == "game":
         desenha_tela(tabuleiro)
         if not has_ia: player()
@@ -166,7 +203,15 @@ while game_loop:
             else:
                 ai_player.set_next_move()
                 peca, move = ai_player.get_next_move()
+                old_pos = peca.get_pos()
                 peca.move(move)
+                new_pos = peca.get_pos()
+
+                white_check = black_check = False
+                if isinstance(peca, King):
+                    if white_turn: w_king_pos = new_pos
+                    else: b_king_pos = new_pos
+
                 if isinstance(peca, Pawn) and (peca.get_row() in [0, 7]):
                     peca.promote(Queen(peca.get_pos_str(), peca.get_is_white()))
                 end_turn()
